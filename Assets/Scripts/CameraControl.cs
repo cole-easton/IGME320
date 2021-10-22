aec86e3bc04f243de7611abfcdb3a0c15a11d587
Matthew Discavage <mpd7516@rit.edu>
2021-10-22 17:09:58 -0400

    Camera mouse controls

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -7,12 +7,15 @@ using UnityStandardAssets.Utility;
 public class CameraControl : MonoBehaviour
 {
     public GameObject target;
-    public float adjustmentSpeed = 50;
+    public float adjustmentSpeed;
+    public float angleAdjustmentSpeed;
     private Vector3 initialPos;
     private Vector3 initialTargetPos;
     private Vector3 cameraRotation;
     private float verticalRadius = 7.5f;
     private float horizontalRadius = 7.5f;
+    private float mouseXInitialValue;
+    private float mouseYInitialValue;
     private int horizontalAngle = 270;
     private int verticalAngle = 20;
     private int verticalAimingAngle = 10;
@@ -22,12 +25,36 @@ public class CameraControl : MonoBehaviour
         initialPos = new Vector3(horizontalRadius * Mathf.Cos(horizontalAngle * Mathf.PI / 180), 10, horizontalRadius * Mathf.Sin(horizontalAngle * Mathf.PI / 180));
         gameObject.transform.position = initialPos;
         initialTargetPos = target.transform.position;
+        mouseXInitialValue = Input.GetAxis("Mouse X");
+        mouseYInitialValue = Input.GetAxis("Mouse Y");
         //transform.rotation = Quaternion.Euler(20, horizontalAngle - 270, 0);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        horizontalAngle -= (int)(angleAdjustmentSpeed * (Input.GetAxis("Mouse X") - mouseXInitialValue));
+        if (!isAiming)
+        {
+            verticalAngle -= (int)(angleAdjustmentSpeed * (Input.GetAxis("Mouse Y") - mouseYInitialValue));
+            if (verticalAngle < 0)
+                verticalAngle = 0;
+            if (verticalAngle > 90)
+                verticalAngle = 90;
+        }
+        else if (isAiming)
+        {
+            verticalAimingAngle += (int)(angleAdjustmentSpeed * (Input.GetAxis("Mouse Y") - mouseYInitialValue));
+            if (verticalAimingAngle < 0)
+                verticalAimingAngle = 0;
+            if (verticalAimingAngle > 90)
+                verticalAimingAngle = 90;
+        }
+
+        /*
         if (Input.GetKey(KeyCode.H))
         {
             horizontalAngle--;
@@ -51,6 +78,7 @@ public class CameraControl : MonoBehaviour
             else if (isAiming && verticalAimingAngle >= 0)
                 verticalAimingAngle--;
         }
+        */
 
         if (Input.GetKeyDown(KeyCode.Y))
         {
