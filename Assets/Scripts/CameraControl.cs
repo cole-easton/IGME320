063cc267d61573a7311c3e3a1dfd942584292556
Matthew Discavage <mpd7516@rit.edu>
2021-10-21 20:30:24 -0400

    Added aiming camera functionality

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -15,6 +15,8 @@ public class CameraControl : MonoBehaviour
     private float horizontalRadius = 7.5f;
     private int horizontalAngle = 270;
     private int verticalAngle = 20;
+    private int verticalAimingAngle = 10;
+    private bool isAiming = false;
     void Start()
     {
         initialPos = new Vector3(horizontalRadius * Mathf.Cos(horizontalAngle * Mathf.PI / 180), 10, horizontalRadius * Mathf.Sin(horizontalAngle * Mathf.PI / 180));
@@ -28,28 +30,52 @@ public class CameraControl : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.H))
         {
-            horizontalAngle++;
+            horizontalAngle--;
         }
         if (Input.GetKey(KeyCode.F))
         {
-            horizontalAngle--;
+            horizontalAngle++;
         }
 
-        if (Input.GetKey(KeyCode.T) && verticalAngle <= 90)
+        if (Input.GetKey(KeyCode.T))
         {
-            verticalAngle++;
+            if (!isAiming && verticalAngle <= 90)
+                verticalAngle++;
+            else if (isAiming && verticalAimingAngle <= 90)
+                verticalAimingAngle++;
         }
-        if (Input.GetKey(KeyCode.G) && verticalAngle >= 0)
+        if (Input.GetKey(KeyCode.G))
         {
-            verticalAngle--;
+            if (!isAiming && verticalAngle >= 0)
+                verticalAngle--;
+            else if (isAiming && verticalAimingAngle >= 0)
+                verticalAimingAngle--;
         }
 
-        transform.rotation = Quaternion.Euler(verticalAngle, -(horizontalAngle - 270), 0);
-        horizontalRadius = verticalRadius * Mathf.Cos(verticalAngle * Mathf.PI / 180);
-        initialPos = new Vector3(horizontalRadius * Mathf.Cos(horizontalAngle * Mathf.PI / 180),
-            verticalRadius * Mathf.Sin(verticalAngle * Mathf.PI / 180) + 7.5f,
-            horizontalRadius * Mathf.Sin(horizontalAngle * Mathf.PI / 180));
-        Vector3 goalPos = initialPos + target.transform.position - initialTargetPos;
-        transform.position += (goalPos - transform.position) * adjustmentSpeed * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            isAiming = isAiming ? false : true;            
+        }
+
+        if (!isAiming)
+        {
+            transform.rotation = Quaternion.Euler(verticalAngle, -(horizontalAngle - 270), 0);
+            horizontalRadius = verticalRadius * Mathf.Cos(verticalAngle * Mathf.PI / 180);
+            initialPos = new Vector3(horizontalRadius * Mathf.Cos(horizontalAngle * Mathf.PI / 180),
+                verticalRadius * Mathf.Sin(verticalAngle * Mathf.PI / 180) + 7.5f,
+                horizontalRadius * Mathf.Sin(horizontalAngle * Mathf.PI / 180));
+            Vector3 goalPos = initialPos + target.transform.position - initialTargetPos;
+            transform.position += (goalPos - transform.position) * adjustmentSpeed * Time.deltaTime;
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(-verticalAimingAngle, -(horizontalAngle - 270), 0);
+            horizontalRadius = verticalRadius * Mathf.Cos(verticalAngle * Mathf.PI / 180);
+            initialPos = new Vector3(-horizontalRadius / 7.5f * Mathf.Cos(horizontalAngle * Mathf.PI / 180),
+                verticalRadius / 7.5f * Mathf.Sin(verticalAimingAngle * Mathf.PI / 180) + 7.5f,
+                -horizontalRadius / 7.5f * Mathf.Sin(horizontalAngle * Mathf.PI / 180));
+            Vector3 goalPos = initialPos + target.transform.position - initialTargetPos;
+            transform.position += (goalPos - transform.position) * adjustmentSpeed * Time.deltaTime;
+        }
     }
 }
